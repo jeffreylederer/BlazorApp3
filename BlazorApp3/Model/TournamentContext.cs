49566fb251e74c7920195c3cf077c8c0c66963b9
Jeffrey Lederer <jeffrey@winnlederer.com>
2024-06-20 14:20:34 -0400

    Update TournamentContext.cs

--- BlazorApp3/Model/TournamentContext.cs
+++ BlazorApp3/Model/TournamentContext.cs
@@ -44,10 +44,6 @@ public partial class TournamentContext : DbContext, IUnitOfWork
 
     public virtual DbSet<UserLeagueView> UserLeagueViews { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=newdell\\SQLEXPRESS;Database=NewTournament;Trusted_Connection=True;Password=burnt#end1;User ID=AdminUser;Encrypt=False;TrustServerCertificate=False;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<ElmahError>(entity =>
