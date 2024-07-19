77abfce7780c0bd1ea44c114aadfe089b375af04
Jeffrey Lederer <jeffrey@winnlederer.com>
2024-07-18 21:24:35 -0400

    Replaced after render to  inialied

---
+++ BlazorApp3/NewModels/GetMatchAllTable.cs
@@ -0,0 +1,25 @@
+﻿using BlazorApp3.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BlazorApp3.NewModels
+{
+    public class GetMatchAllTable
+    {
+        public int? Id {get; set; }
+        public string? Skip1 {get; set; }
+        public string? Vice1 {get; set; }
+        public string? Lead1 {get; set; }
+        public string? Skip2 {get; set; }
+        public string? Vice2 {get; set; }
+        public string? Lead2 {get; set; }
+        public string? Team1 {get; set; }
+        public string? Team2 {get; set; }
+        public DateOnly Date {get; set; }
+        public int Rink {get; set; }
+        public string? Division1 {get; set; }
+        public string? Division2 {get; set; }
+    }
+
+    
+}
